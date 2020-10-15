a2c6f23c78b0cad9cede57f0daa5cb07bb440868
Fred Nordell <dat15fno@student.lu.se>
2020-10-15 12:49:19 +0200

    started fix csrf attack

--- homepage.php
+++ homepage.php
@@ -5,15 +5,17 @@
 session_start();
 
 if(isset($_SESSION["loggedin"])&& isset($_SESSION["username"])){
-    $url = 'http://localhost/EITF05-unsafe/login/change-pwd.php';
-    $data = array('password' => 'hejhej', 'confirm_password' => 'hejhej');
+    $url = 'https://localhost/EITF05-unsafe/login/change-pwd.php';
+    $data = array('new_password' => 'hejhejhejhejhejheJ123', 'confirm_password' => 'hejhejhejhejhejheJ123');
 
     // use key 'http' even if you send the request to https://...
     $options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
+    'ssl' => array(
+        'header'            => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'            => 'POST',
+        'content'           => http_build_query($data),
+        "verify_peer"       => false,
+        "verify_peer_name"  => false
     )
     );
     $context  = stream_context_create($options);
