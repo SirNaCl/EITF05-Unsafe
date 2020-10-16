05e144116c0c1a03393ceda833252b807c5efeb4
Fred Nordell <dat15fno@student.lu.se>
2020-10-16 09:47:05 +0200

    fixed csrf

--- homepage.php
+++ homepage.php
@@ -6,30 +6,27 @@ session_start();
 
 if(isset($_SESSION["loggedin"])&& isset($_SESSION["username"])){
     $url = 'https://localhost/EITF05-unsafe/login/change-pwd.php';
-    $data = array('new_password' => 'hejhejhejhejhejheJ123', 'confirm_password' => 'hejhejhejhejhejheJ123');
+    $data = array('password' => 'asdasdB1', 'confirm_password' => 'asdasdB1');
 
-    // use key 'http' even if you send the request to https://...
     $options = array(
-    'ssl' => array(
+      'http' => array(
         'header'            => "Content-type: application/x-www-form-urlencoded\r\n",
         'method'            => 'POST',
         'content'           => http_build_query($data),
+      ),
+      'ssl' => array(
         "verify_peer"       => false,
         "verify_peer_name"  => false
-    )
+      )
     );
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
     if ($result === FALSE) { /* Handle error */ }
 
-
     var_dump($result);
-
     echo "<script type='text/javascript'>
                    document.getElementById('subbtn').click();
                  </script>";
-
-
 
 }else{
   header("location: index.php");
